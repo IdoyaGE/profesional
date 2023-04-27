5bbde3d610d192963484014a32a61b59d568ffd8
IdoyaGE <igearkabaia@gmail.com>
2023-04-27 23:42:31 +0200

    primer commit creación componente Navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';//importo el CSS general
+import Navbar from './Navbar/Navbar'; //importo el componente Navbar y lo añado en el div
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
